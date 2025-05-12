402fe35f933cb270acfc1a2e672f18cc7af2f002
Lukasbartas <lukas.bagdavicius@gmail.com>
2025-05-12 17:41:38 +0300

    Initial commit

---
+++ Assets/Scripts/BouncyBall.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyBall : MonoBehaviour
+{
+    public float minY = -5.5f;
+    public float maxVelocity = 15f;
+
+    Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    
+    void Update()
+    {
+        if(transform.position.y < minY)
+        {
+            transform.position = Vector3.zero;
+            rb.velocity = Vector3.zero;
+        }
+        if(rb.velocity.magnitude > maxVelocity)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+        }
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
